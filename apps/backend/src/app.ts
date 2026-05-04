cda4d70d856d96d9283e8f34c27525e3b51ef351
Felipe Cruz <felp.cruz.comp@gmail.com>
2026-05-04 13:05:28 -0300

    other: elysia retunrs log update

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -36,7 +36,7 @@ const app = new Elysia()
   .use(appRoutes)
   .listen(port);
 
-console.log(`🔊 Listening on http://localhost:${app.server?.port}`);
-console.log(`📃 Docs:      http://localhost:${app.server?.port}/swagger`);
+console.log(`🔊 ... Listening: http://localhost:${app.server?.port}`);
+console.log(`📃 ... Docs: http://localhost:${app.server?.port}/docs`);
 
 export type App = typeof app;
